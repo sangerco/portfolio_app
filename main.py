4ffaadcb7c3f869b89385e45bee12c601c7d2dce
sangerco <sangerco@yahoo.com>
2024-03-26 15:58:26 -0600

    adding content

--- main.py
+++ main.py
@@ -5,11 +5,20 @@ st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image('images/photo.png')
+    st.image('images/Daddy_Finn_2022.jpg')
 
 with col2:
     st.title('Dan Sanger')
     content = """
-    I will fill this in later!
+    Hi, I'm Dan! I am a freelance software developer based in Longmont, Colorado. I am a father, avid skier, hiker,
+    and lover of all things outdoors. I have previously been the owner and co-founder of Wild Tree Naturals, LLC, 
+    an organic bath and body care company. My goal is to find a software development position with a dynamic 
+    organization where I can put my multiple tech skills, business experience, and soft skills to good use!
     """
     st.info(content)
+
+
+content2 = """
+Below you can find some of the apps that I have created! Feel free to contact me!
+"""
+st.write(content2)
