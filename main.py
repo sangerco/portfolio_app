85b951954c2ae01fa040fb7ab41cf73a696b6c24
sangerco <sangerco@yahoo.com>
2024-03-26 16:33:02 -0600

    add columns with descriptions, images, and links

--- main.py
+++ main.py
@@ -24,14 +24,24 @@ Below you can find some of the apps that I have created! Feel free to contact me
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv('data.csv', sep=";")
+
+url = 'https://github.com/sangerco'
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        # st.write(f"[Source Code]({row['url']})")
+        st.write(f"[Source Code]({url})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        # st.write(f"[Source Code]({row['url']})")
+        st.write(f"[Source Code]({url})")
